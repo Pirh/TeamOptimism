f32c5d3bb53118722c7045aeb5ad4971a7d91c9a
Ryan Davies <ryanrenniedavies@gmail.com>
2013-02-21 15:09:56 +0000

    Health Component

--- src/com/optimism/components/Health.java
+++ src/com/optimism/components/Health.java
@@ -1,5 +1,19 @@
 package com.optimism.components;
 
-public class Health {
+import com.artemis.Component;
 
+public class Health extends Component{
+	private int health;
+	
+	public Health(int h){
+		this.health = h;
+	}
+	
+	public void gain(int i){
+		this.health = this.health + i;
+	}
+	
+	public void lose(int i){
+		this.health = this.health - i;
+	}
 }
